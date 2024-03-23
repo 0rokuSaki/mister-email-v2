962503d4b0383de0b257f3f21b982c1b229f47f7
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-03-23 20:16:25 +0200

    Added basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-
-import { Home } from './pages/Home';
 export function App() {
+  return <section className="app">
+    <section className="main-layout">
+      <aside>
+        
+      </aside>
+      <main>
 
-    return (
-        <section className='main-app'>
-            <header className="app-header">
-                <section className="container">
-                    <h1>Log111</h1>
-                </section>
-            </header>
-
-            <main className='container'>
-                <Home />
-            </main>
-
-            <footer>
-                <section className="container">
-                    robotRights 2023 &copy;
-                </section>
-            </footer>
-        </section>
-
-
-    )
+      </main>
+    </section>
+  </section>;
 }
-
